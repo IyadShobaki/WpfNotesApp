f42e52d2e067ec2a87223eeb60eb4115a884d27d
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-09 22:54:25 -0400

    The View Model
    Deleting the mainWindow.xaml.
    Add View Folder and create Login and Notes windows.
    Added commands to the viewmodels.

---
+++ WpfUI/ViewModel/LoginVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUI.Model;
+using WpfUI.ViewModel.Commands;
+
+namespace WpfUI.ViewModel
+{
+    public class LoginVM
+    {
+        private User _user;
+
+        public User User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+        public RegisterCommand RegisterCommand { get; set; }
+
+        public LoginCommand LoginCommand { get; set; }
+
+        public LoginVM()
+        {
+            RegisterCommand = new RegisterCommand(this);
+            LoginCommand = new LoginCommand(this);
+        }
+    }
+}
---
+++ WpfUI/ViewModel/NotesVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUI.Model;
+using WpfUI.ViewModel.Commands;
+
+namespace WpfUI.ViewModel
+{
+    public class NotesVM
+    {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+
+        private Notebook _selectedNotebook;
+
+        public Notebook SelectedNotebook
+        {
+            get { return _selectedNotebook; }
+            set
+            {
+                _selectedNotebook = value; 
+                //TODO - Get notes
+            }
+        }
+
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+
+        public NewNoteCommand NewNoteCommand { get; set; }
+
+        public NotesVM()
+        {
+            NewNotebookCommand = new NewNotebookCommand(this);
+            NewNoteCommand = new NewNoteCommand(this);
+        }
+    }
+}
