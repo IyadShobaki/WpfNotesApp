fd97c52e25209f61502c8a3079dd86948a9bdae8
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-10 20:18:17 -0400

    Add Functionality to Speech Button
    Add reference to System.speech library and work with speech recognizer.

--- WpfUI/View/NotesWindow.xaml.cs
+++ WpfUI/View/NotesWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Recognition;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +21,33 @@ namespace WpfUI.View
     /// </summary>
     public partial class NotesWindow : Window
     {
+        SpeechRecognitionEngine recognizer;
+
         public NotesWindow()
         {
             InitializeComponent();
+
+            var currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
+                                 where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+                                 select r).FirstOrDefault();
+            recognizer = new SpeechRecognitionEngine(currentCulture);
+
+            GrammarBuilder builder = new GrammarBuilder();
+
+            builder.AppendDictation();
+            Grammar grammar = new Grammar(builder);
+
+            recognizer.LoadGrammar(grammar);
+            recognizer.SetInputToDefaultAudioDevice();
+
+            recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string recognizedText = e.Result.Text;
+
+            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recognizedText)));
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -29,9 +55,20 @@ namespace WpfUI.View
             Application.Current.Shutdown();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        bool IsRecognizing = false;
+        private void speechButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (IsRecognizing == false)
+            {
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                IsRecognizing = true;
+            }
+            else
+            {
+                recognizer.RecognizeAsyncStop();
+                IsRecognizing = false;
+            }
+            
         }
 
         private void contentRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
